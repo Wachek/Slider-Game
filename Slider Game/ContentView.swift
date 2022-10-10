936a472d8398f096a81c5105d90904d78d02267d
Wachek <wachek7@gmail.com>
2022-10-10 21:34:16 +0300

    Initial Commit

---
+++ Slider Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slider Game
+//
+//  Created by Вячеслав Турчак on 10.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Slider Game/Slider_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slider_GameApp.swift
+//  Slider Game
+//
+//  Created by Вячеслав Турчак on 10.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Slider_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
